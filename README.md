981c1d6a751559c3a815cbf1a6f045d3cb858da1
Juan Pablo Calao Perez <jcalaop@gmail.com>
2020-02-11 22:37:39 -0500

    finalize primer excersice

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ This is an exercise to practice basic programming principles.
 
 1. Define an `addFavoriteBook(..)` function that receives a single parameter, called `bookName`.
 2. If the provided `bookName` string does **NOT** have the word "Great" in it, add it to the `favoriteBooks` array.
+3. Define a `printFavoriteBooks()` function that receive no paraeters.
+4. `printFavoriteBooks()` should first print a message like "Favorite Books: ...", and include the number of books in the `favoriteBooks` array.
+5. Finally, `printFavoriteBooks()` should loop though the `favoriteBooks` array and print each value.
--- progPrimerExc.js
+++ progPrimerExc.js
@@ -2,6 +2,7 @@
 
 const favoriteBooks = [];
 
+// Define addFavoriteBook(...)
 function addFavoriteBook(bookName) {
     if(!bookName.toLowerCase().includes("great")) {
         favoriteBooks.push(bookName);
@@ -14,4 +15,15 @@ addFavoriteBook("Crime & Punishment");
 addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
-console.log(favoriteBooks);
+// Define printFavoriteBook(...)
+
+function printFavoriteBook() {
+    if (favoriteBooks.length > 0) {
+        console.log(`Favorite Books: ${favoriteBooks.length}\n`)
+        favoriteBooks.forEach(bookName => console.log(`- ${bookName}`))
+    } else {
+        console.log("There are no Favorite Books")
+    }
+}
+
+printFavoriteBook()
